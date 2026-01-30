a00e2029cb6899eb5b81ce523cda673e5f409af8
evarasvb <evaras@firmavb.cl>
2026-01-30 03:17:23 -0300

    Update ComprasAgilesTable.tsx

--- src/components/compras-agiles/ComprasAgilesTable.tsx
+++ src/components/compras-agiles/ComprasAgilesTable.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
